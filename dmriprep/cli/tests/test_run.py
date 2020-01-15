49502baa3ddc43c157baf7ca5adfd35fa355452d
Lawreros <lawreros@gmail.com>
2020-01-15 20:41:06 +0000

    run and bids tests, rough drafts

--- dmriprep/cli/tests/test_run.py
+++ dmriprep/cli/tests/test_run.py
@@ -3,7 +3,7 @@ from packaging.version import Version
 import pytest
 from .. import version as _version
 from ... import __about__
-from ..run import get_parser
+from ..run import get_parser, check_deps
 
 
 @pytest.mark.parametrize(('current', 'latest'), [
@@ -49,3 +49,96 @@ def test_get_parser_blacklist(monkeypatch, capsys, flagged):
     assert ('FLAGGED' in captured) is flagged[0]
     if flagged[0]:
         assert ((flagged[1] or 'reason: unknown') in captured)
+
+def test_check_deps(input_dir_tree):
+    from dmriprep.cli.run import build_workflow
+    from pathlib import Path
+    import logging
+    import os
+    import sys
+    from nipype import logging as nlogging
+    from multiprocessing import set_start_method, Process, Manager
+    from dmriprep.utils.bids import write_derivative_description, validate_input_dir
+
+    tmp_path, data = input_dir_tree
+
+    set_start_method('forkserver')
+    sys.argv=[str(tmp_path),str(tmp_path)+'/derivatives',"participant"]
+    opts = get_parser().parse_args(sys.argv)
+
+    exec_env = os.name
+
+    # special variable set in the container
+    if os.getenv('IS_DOCKER_8395080871'):
+        exec_env = 'singularity'
+        cgroup = Path('/proc/1/cgroup')
+        if cgroup.exists() and 'docker' in cgroup.read_text():
+            exec_env = 'docker'
+            if os.getenv('DOCKER_VERSION_8395080871'):
+                exec_env = 'dmriprep-docker'
+
+    #sentry_sdk = None
+    #if not opts.notrack:
+    #    import sentry_sdk
+    #    from dmriprep.utils.sentry import sentry_setup
+    #    sentry_setup(opts, exec_env)
+
+    # Validate inputs
+    if not opts.skip_bids_validation:
+        print("Making sure the input data is BIDS compliant (warnings can be ignored in most "
+              "cases).")
+        validate_input_dir(exec_env, opts.bids_dir, opts.participant_label)
+
+    # FreeSurfer license
+    default_license = str(Path(os.getenv('FREESURFER_HOME')) / 'license.txt')
+    # Precedence: --fs-license-file, $FS_LICENSE, default_license
+    license_file = opts.fs_license_file or Path(os.getenv('FS_LICENSE', default_license))
+    if not license_file.exists():
+        raise RuntimeError("""\
+ERROR: a valid license file is required for FreeSurfer to run. dMRIPrep looked for an existing \
+license file at several paths, in this order: 1) command line argument ``--fs-license-file``; \
+2) ``$FS_LICENSE`` environment variable; and 3) the ``$FREESURFER_HOME/license.txt`` path. Get it \
+(for free) by registering at https://surfer.nmr.mgh.harvard.edu/registration.html""")
+    os.environ['FS_LICENSE'] = str(license_file.resolve())
+
+    # Retrieve logging level
+    log_level = int(max(25 - 5 * opts.verbose_count, logging.DEBUG))
+    # Set logging
+    logger = logging.getLogger('cli')
+    logger.setLevel(log_level)
+    nlogging.getLogger('nipype.workflow').setLevel(log_level)
+    nlogging.getLogger('nipype.interface').setLevel(log_level)
+    nlogging.getLogger('nipype.utils').setLevel(log_level)
+
+    # Call build_workflow(opts, retval)
+    with Manager() as mgr:
+        retval = mgr.dict()
+        p = Process(target=build_workflow, args=(opts, retval))
+        p.start()
+        p.join()
+
+        retcode = p.exitcode or retval.get('return_code', 0)
+        bids_dir = Path(retval.get('bids_dir'))
+        output_dir = Path(retval.get('output_dir'))
+        work_dir = Path(retval.get('work_dir'))
+        plugin_settings = retval.get('plugin_settings', None)
+        subject_list = retval.get('subject_list', None)
+        dmriprep_wf = retval.get('workflow', None)
+        run_uuid = retval.get('run_uuid', None)
+
+    if opts.reports_only:
+        sys.exit(int(retcode > 0))
+
+    if opts.boilerplate:
+        sys.exit(int(retcode > 0))
+
+    if dmriprep_wf and opts.write_graph:
+        dmriprep_wf.write_graph(graph2use="colored", format='svg', simple_form=True)
+
+    retcode = retcode or int(dmriprep_wf is None)
+    if retcode != 0:
+        sys.exit(retcode)
+
+    # Check workflow for missing commands
+    missing = check_deps(dmriprep_wf)
+    assert missing==[]
--- dmriprep/utils/tests/test_bids.py
+++ dmriprep/utils/tests/test_bids.py
@@ -4,52 +4,47 @@ import pytest
 from pathlib import Path
 import bids
 import json
-from dmriprep.utils.bids import collect_data
+from dmriprep.utils.bids import collect_data, write_derivative_description, validate_input_dir
 
 
-@pytest.fixture
-def input_dir_tree(tmp_path):
-    data = {"002547": [1,2], "002548": [1], "002449": [1,2,3]}
-    for sub, session in data.items():
-        for ses in session:
-            info = f"sub-{sub}/ses-{ses}"
-            anat = os.path.join(tmp_path, info, "anat")
-            dwi = os.path.join(tmp_path, info, "dwi")
-            # make directories and files
-            os.makedirs(anat)
-            os.makedirs(dwi)
-            tmpfilepath = os.path.join(anat, f"sub-{sub}_ses-{ses}_T1w.nii.gz")
-            with open(tmpfilepath, "x") as f:
-                f.write("placeholder text")
-            tmpfilepath = os.path.join(dwi, f"sub-{sub}_ses-{ses}_dwi.bval")
-            with open(tmpfilepath, "x") as f:
-                f.write("placeholder text")
-            tmpfilepath = os.path.join(dwi, f"sub-{sub}_ses-{ses}_dwi.bvec")
-            with open(tmpfilepath, "x") as f:
-                f.write("placeholder text")
-            tmpfilepath = os.path.join(dwi, f"sub-{sub}_ses-{ses}_dwi.nii.gz")
-            with open(tmpfilepath, "x") as f:
-                f.write("placeholder text")
-    #with open(os.path.join(tmp_path,"dataset_description.json"), "x") as f:
-    #    f.write("placeholder text")
 
-    name = Path(tmp_path).stem
+
+
+def test_collect_data(dipy_test_data):
+    # Only asserts that collect_data runs, need to actually check the structure of the variable
+    bvals = dipy_test_data['bvals']
+    _dipy_datadir_root = os.getenv('DMRIPREP_TESTS_DATA') or Path.home()
+    dipy_datadir = Path(_dipy_datadir_root) / '.cache' / 'data'
+    #dipy_datadir.mkdir(parents=True, exist_ok=True)
+    #tmp_path, subjects = input_dir_tree
+
+    name = Path(dipy_datadir).stem
     vers = bids.__version__
     out = dict(Name=name, BIDSVersion=vers)
-    with open(os.path.join(tmp_path, "dataset_description.json"), "x") as f:
+    with open(os.path.join(dipy_datadir, "dataset_description.json"), "w") as f:
         f.write('{"BIDSVersion": "1.0.0", "License": "This dataset is made available under the Public Domain Dedication and License v1.0, whose full text can be found at www.opendatacommon We hope that all users will follow the ODC Attribution/Share-Aliommunityin particular, while not legally required, we hope that all users of the data will acknowledge the OpenfMRI project and NSF Grant OCI-1131441  Poldrack, PI in any publications.","Name": "Prefrontal-Subcortical Pathways Mediating Successful Emotion Regulation","ReferencesAndLinks": ["Wager, T.D., Davidson, M.L., Hughes, B.L., Lindquist, M.A., Ochsner, K.N. (2008). Prefrontal-subcortical pathways mediating successful emotion regulation. Neuron, 59(6):1037-50. doi: 10.1016/j.neuron.2008.09.006"]}')
 
-    # Create non-BIDS files
-    tmpfilepath = os.path.join(anat, f"dummy.txt")
-    with open(tmpfilepath, "x") as f:
-        f.write("placeholder text")
-    tmpfilepath = os.path.join(dwi, f"sub-55.nii.gz")
-    with open(tmpfilepath, "x") as f:
-        f.write("placeholder text")
+    subj_data, layout = collect_data(dipy_datadir, "data")
+    assert subj_data
+    assert layout
 
-    return tmp_path, data
-
-
-def test_collect_data(input_dir_tree):
+def test_write_derivative_description(input_dir_tree):
     tmp_path, subjects = input_dir_tree
-    subj_data, layout = collect_data(tmp_path, "sub-002548")
+    write_derivative_description(tmp_path, tmp_path)
+    desc_file=tmp_path / 'dataset_description.json'
+    assert desc_file.exists()
+
+def test_validate_input_dir(input_dir_tree):
+    tmp_path, subjects = input_dir_tree
+    exec_env = os.name
+
+    # special variable set in the container
+    if os.getenv('IS_DOCKER_8395080871'):
+        exec_env = 'singularity'
+        cgroup = Path('/proc/1/cgroup')
+        if cgroup.exists() and 'docker' in cgroup.read_text():
+            exec_env = 'docker'
+            if os.getenv('DOCKER_VERSION_8395080871'):
+                exec_env = 'dmriprep-docker'
+
+    validate_input_dir(exec_env, tmp_path, None) #Need to figure out issue with subject not being found
